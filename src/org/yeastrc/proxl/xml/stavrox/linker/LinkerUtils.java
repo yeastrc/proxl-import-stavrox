506f8e29b5681dbfcaaf81a44679aaecce576100
Michael Riffle <mriffle@uw.edu>
2016-02-26 15:43:10 -0800

    Working on linker logic.

--- src/org/yeastrc/proxl/xml/stavrox/linker/LinkerUtils.java
+++ src/org/yeastrc/proxl/xml/stavrox/linker/LinkerUtils.java
@@ -1,5 +1,32 @@
 package org.yeastrc.proxl.xml.stavrox.linker;
 
+import org.yeastrc.proxl.xml.stavrox.AnalysisProperties;
+
 public class LinkerUtils {
 
+	/**
+	 * Calculate the mass of the supplied crosslinker using its atomic formula, using the masses
+	 * provided for those atoms in the stavrox properties file.
+	 * 
+	 * @param linker
+	 * @param properties
+	 * @return
+	 * @throws Exception
+	 */
+	public double calculateLinkerMass( StavroxCrosslinker linker, AnalysisProperties properties ) throws Exception {
+		double mass = 0;
+		boolean negative = false;
+		
+		String formula = properties.getCrosslinker().getFormula();
+		if( formula.startsWith( "-" ) ) {
+			negative = true;
+			formula = formula.substring( 1 );			
+		}
+
+		
+		
+		
+		return mass;
+	}
+	
 }
